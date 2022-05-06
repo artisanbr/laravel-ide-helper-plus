34aeded610458021eb722f447c091573562615de
renalcio <renalcio.c@gmail.com>
2022-05-06 13:01:44 -0300

    Console fix

---
+++ src/Console/IdeHelperCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Renalcio\LaravelIdeHelperPlus\Console;
+
+use Illuminate\Console\Command;
+
+class IdeHelperCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'idehelper';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate All IDE Helper';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->call('config:clear');
+        $this->call('ide-helper:generate');
+        $this->call('ide-helper:meta');
+        $this->call('ide-helper:models', ['--nowrite' => 1]);
+        $this->call('cache:clear');
+    }
+}
