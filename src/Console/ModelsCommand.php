b7d63b3379a7d9daa14adcbb45b071e04ab43d0e
artisanbr <github@artisan.dev.br>
2023-11-18 20:51:42 -0300

    0.8.0 - Wordpress Import Tools (em andamento) - Melhoria nas Categorias (em andamento) - Melhoria no cadastro de Temas (em andamento)  - Novo Gerenciador de arquivos no Tema (em andamento)  - Nova gestão de Scripts vinculados ao carregamento do site no Tema (em andamento)

--- src/Console/ModelsCommand.php
+++ src/Console/ModelsCommand.php
@@ -1,13 +1,18 @@
 <?php
+/*
+ * Copyright (c) 2023. Tanda Interativa - Todos os Direitos Reservados
+ * Desenvolvido por Renalcio Carlos Jr.
+ */
 
 namespace ArtisanLabs\LaravelIdeHelperPlus\Console;
 
+use Barryvdh\LaravelIdeHelper\Console\ModelsCommand as ModelsCommandBase;
 use Composer\Autoload\ClassMapGenerator;
+use Illuminate\Database\Eloquent\Casts\AsCollection;
 use Illuminate\Support\Str;
 use ReflectionClass;
 use Symfony\Component\Console\Output\OutputInterface;
 use Throwable;
-use Barryvdh\LaravelIdeHelper\Console\ModelsCommand as ModelsCommandBase;
 
 
 class ModelsCommand extends ModelsCommandBase
@@ -137,6 +142,118 @@ class ModelsCommand extends ModelsCommandBase
         return $output;
     }
 
+    public function castPropertiesType($model)
+    {
+        $casts = $model->getCasts();
+        foreach ($casts as $name => $type) {
+            if (Str::startsWith($type, 'decimal:')) {
+                $type = 'decimal';
+            } elseif (Str::startsWith($type, 'custom_datetime:')) {
+                $type = 'date';
+            } elseif (Str::startsWith($type, 'date:')) {
+                $type = 'date';
+            } elseif (Str::startsWith($type, 'datetime:')) {
+                $type = 'date';
+            } elseif (Str::startsWith($type, 'immutable_custom_datetime:')) {
+                $type = 'immutable_date';
+            } elseif (Str::startsWith($type, 'encrypted:')) {
+                $type = Str::after($type, ':');
+            }
+
+            $params = [];
+
+            switch ($type) {
+                case 'encrypted':
+                    $realType = 'mixed';
+                    break;
+                case 'boolean':
+                case 'bool':
+                    $realType = 'boolean';
+                    break;
+                case 'decimal':
+                case 'string':
+                    $realType = 'string';
+                    break;
+                case 'array':
+                case 'json':
+                    $realType = 'array';
+                    break;
+                case 'object':
+                    $realType = 'object';
+                    break;
+                case 'int':
+                case 'integer':
+                case 'timestamp':
+                    $realType = 'integer';
+                    break;
+                case 'real':
+                case 'double':
+                case 'float':
+                    $realType = 'float';
+                    break;
+                case 'date':
+                case 'datetime':
+                    $realType = $this->dateClass;
+                    break;
+                case 'immutable_date':
+                case 'immutable_datetime':
+                    $realType = '\Carbon\CarbonImmutable';
+                    break;
+                case 'collection':
+                    $realType = '\Illuminate\Support\Collection';
+                    break;
+                default:
+
+                    if(Str::contains($type, ':')){
+
+                        $casterClass = collect(explode(':', $type))->first();
+                        $finalClass = collect(explode(':', $type))->last();
+
+                        if(class_exists($casterClass) && class_exists($finalClass)){
+                            $realType = $type;
+                            break;
+                        }
+
+                    }
+
+                    // In case of an optional custom cast parameter , only evaluate
+                    // the `$type` until the `:`
+                    $type = strtok($type, ':');
+                    $realType = class_exists($type) ? ('\\' . $type) : ($type != 'NULL' ? $type : 'mixed');
+
+                    if (!isset($this->properties[$name])) {
+                        //dd($realType);
+                        $this->setProperty($name, null, true, true);
+                    }
+
+                    $params = strtok(':');
+                    $params = $params ? explode(',', $params) : [];
+
+                    break;
+            }
+
+            if (!isset($this->properties[$name])) {
+                continue;
+            }
+            if ($this->isInboundCast($realType)) {
+                continue;
+            }
+
+            $realType = $this->checkForCastableCasts($realType, $params);
+            $realType = $this->checkForCustomLaravelCasts($realType);
+            $realType = $this->getTypeOverride($realType);
+            $this->properties[$name]['type'] = $this->getTypeInModel($model, $realType);
+
+            if(empty($this->properties[$name]['type'])){
+                $this->properties[$name]['type'] = $this->getTypeInModel($model, $type);
+            }
+
+            if (isset($this->nullableColumns[$name])) {
+                $this->properties[$name]['type'] .= '|null';
+            }
+        }
+    }
+
 /*
     protected function loadModels()
     {
@@ -180,6 +297,148 @@ class ModelsCommand extends ModelsCommandBase
      * @param \Illuminate\Database\Eloquent\Model $model
      */
     protected function getPropertiesFromAttributes($model)
+    {
+        $fillables_model = $model->getFillable() ?? [];
+        $attributes_model = $model->getAttributes();
+        $fillables = [];
+
+
+
+        foreach($fillables_model as $attribute => $default_value){
+            $name = is_string($attribute) ? $attribute : $default_value;
+            $value = is_string($attribute) ? $default_value : ($attributes_model[$name] ?? null);
+
+            $fillables[$name] = $value;
+        }
+
+        $attributes = array_merge($fillables, $attributes_model);
+        $casts = $model->getCasts();
+
+        /*if(!is_subclass_of($model, \Eloquent::class)){
+            dd($casts);
+        }*/
+
+        foreach ($attributes as $attribute => $default_value) {
+            $name = is_string($attribute) ? $attribute : $default_value;
+            $value = is_string($attribute) ? $default_value : null;
+            if (in_array($name, $model->getDates())) {
+                $type = $this->dateClass;
+            } else {
+                $type = $casts[$name] ?? gettype($default_value);
+                switch ($type) {
+                    case 'string':
+                    case 'text':
+                    case 'date':
+                    case 'time':
+                    case 'guid':
+                    case 'datetimetz':
+                    case 'datetime':
+                    case 'decimal':
+                        $type = 'string';
+                        break;
+                    case 'integer':
+                    case 'bigint':
+                    case 'smallint':
+                        $type = 'integer';
+                        break;
+                    case 'boolean':
+                        switch (config('database.default')) {
+                            case 'sqlite':
+                            case 'mysql':
+                                $type = 'integer';
+                                break;
+                            default:
+                                $type = 'boolean';
+                                break;
+                        }
+                        break;
+                    case 'float':
+                        $type = 'float';
+                        break;
+                    case 'collection':
+                        $type = '\Illuminate\Support\Collection';
+                        break;
+                    default:
+
+                        /*switch (true){
+                            case Str::contains($type, ':'):
+                                $type = collect(explode(':', $type))->last();
+                                dd($type);
+                                break;
+                            default:
+                                $type = 'mixed';
+                                break;
+                        }*/
+
+                        if($type == 'NULL'){
+                            $type = 'mixed';
+                        }
+
+                        break;
+                }
+            }
+
+            $this->nullableColumns[$name] = true;
+
+            $this->setProperty(
+                $name,
+                $this->getTypeInModel($model, $type),
+                true,
+                true,
+                "",
+                true
+            );
+
+            if ($this->write_model_magic_where && is_subclass_of($model, \Eloquent::class)) {
+                $this->setMethod(
+                    Str::camel('where_' . $name),
+                    $this->getClassNameInDestinationFile($model, \Illuminate\Database\Eloquent\Builder::class)
+                    . '|'
+                    . $this->getClassNameInDestinationFile($model, get_class($model)),
+                    ['$value']
+                );
+            }
+        }
+    }
+
+    protected function getTypeInModel(object $model,?string $type) : ?string{
+
+        if(Str::contains($type, ':')){
+
+            $casterClass = collect(explode(':', $type))->first();
+            $finalClass = collect(explode(':', $type))->last();
+
+
+            if(is_subclass_of($casterClass, AsCollection::class)){
+                return "\\Illuminate\\Support\\Collection|\\{$finalClass}[]|\\Illuminate\\Support\\Collection<\\{$finalClass}>";
+            }
+
+            dd('Subcast', $casterClass, $finalClass, $type);
+
+
+            //$type = collect(explode(':', $type))->last();
+        }
+
+        /*if(class_exists($type) && is_subclass_of($type, GenericModel::class)){
+            dd($model, $type, parent::getTypeInModel($model, $type));
+        }*/
+
+        return parent::getTypeInModel($model, $type);
+    }
+
+    protected function createPhpDocs($class){
+
+        $output = parent::createPhpDocs($class);
+
+        if(!is_subclass_of($class, \Eloquent::class)){
+            $extendClass = get_parent_class($class) ?? 'Eloquent';
+            $output = Str::replace('extends \Eloquent', "extends \\{$extendClass}", $output);
+        }
+
+        return $output;
+    }
+
+    protected function getPropertiesFromAttributesOld($model)
     {
         $fillables_model = $model->getFillable() ?? [];
         $attributes_model = $model->getAttributes();
